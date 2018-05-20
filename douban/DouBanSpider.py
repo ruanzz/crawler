df21d2863bfcf1ead7c7b3f5e300e3a7e0c26ff9
ruanzz <rzz1995@live.com>
2018-05-20 20:06:24 +0800

    format

--- douban/DouBanSpider.py
+++ douban/DouBanSpider.py
@@ -1,7 +1,8 @@
 import requests
 import re
-from bs4 import  BeautifulSoup
+from bs4 import BeautifulSoup
 from pyquery import PyQuery as pq
+
 
 def douban_spider_re():
     '''
@@ -9,17 +10,17 @@ def douban_spider_re():
     :return:
     '''
     url = "http://book.douban.com/"
-    html = requests.get(url=url).text;
+    html = requests.get(url=url).text
 
     pattern = re._compile(
-        'li.*?cover.*?href="(.*?)".*?info.*?author">(.*?)(</span>|</div>|</p>).*?</li>', re.S);
+        'li.*?cover.*?href="(.*?)".*?info.*?author">(.*?)(</span>|</div>|</p>).*?</li>', re.S)
 
-    results = re.findall(pattern=pattern, string=html);
+    results = re.findall(pattern=pattern, string=html)
 
     for result in results:
         href, author, temp = result
         author = re.sub("\s", "", author)
-        author = re.sub("作者：", "", author);
+        author = re.sub("作者：", "", author)
         print(href, author)
 
 
@@ -30,18 +31,19 @@ def douban_spider_bs():
     '''
 
     url = "http://book.douban.com/"
-    html = requests.get(url=url).text;
+    html = requests.get(url=url).text
 
-    soup = BeautifulSoup(html,'lxml')
-    infos = soup.select(".info");
+    soup = BeautifulSoup(html, 'lxml')
+    infos = soup.select(".info")
     for info in infos:
-        #print(info)
+        # print(info)
         href = info.find("a").attrs['href']
         book_name = info.find(class_='title').get_text()
         book_name = re.sub("\s", "", book_name)
         author = info.find(class_='author').get_text()
-        author = re.sub("\s","",author)
-        print(href,book_name,author)
+        author = re.sub("\s", "", author)
+        print(href, book_name, author)
+
 
 def douban_spider_pq():
     '''
@@ -49,22 +51,21 @@ def douban_spider_pq():
     :return:
     '''
     url = "http://book.douban.com/"
-    html = requests.get(url=url).text;
+    html = requests.get(url=url).text
 
     doc = pq(html)
     infos = doc('.info')
-    #print(infos)
-    for info in  infos.items():
-        href =  info.find("a").attr('href')
+    # print(infos)
+    for info in infos.items():
+        href = info.find("a").attr('href')
         book_name = info.find(".title").text()
         author = info.find(".author").text()
-        print(href,book_name,author)
+        print(href, book_name, author)
 
 
 if __name__ == '__main__':
+    # douban_spider_re()
 
-    #douban_spider_re()
-
-    #douban_spider_bs()
+    # douban_spider_bs()
 
     douban_spider_pq()
