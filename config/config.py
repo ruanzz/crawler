e6f445b8d5fcac15409d4882ecadd5a6aa4c409e
ruanzz <rzz1995@live.com>
2018-05-22 21:31:10 +0800

    config抽取出来

---
+++ config/config.py
@@ -0,0 +1,15 @@
+# MongoDB
+MONGO_URL = '127.0.0.1'
+MONGO_DB = 'crawler'
+
+############# TOUTIAO ############
+MONGO_TABLE_TOUTIAO = 'toutiao'
+# PAGES
+PAGE_START = 1
+PAGE_END = 5
+# KW
+KEYWORD = '超跑'
+
+############ TAOBAO ###############
+MONGO_TABLE_TAOBAO = 'taobao'
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
--- toutiao/config.py
+++
@@ -1,9 +0,0 @@
-# MongoDB
-MONGO_URL = '127.0.0.1'
-MONGO_DB = 'toutiao'
-MONGO_TABLE = 'toutiao'
-# PAGES
-PAGE_START = 1
-PAGE_END = 5
-# KW
-KEYWORD = '超跑'
